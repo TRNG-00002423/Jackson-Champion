1ebd8afd6a011ca3857c50c5657d6a70b3df608b
Jackson Champion <jackson.champion136@gmail.com>
2026-06-02 18:21:33 -0400

    Recover accidentally deleted files

---
+++ Week1-Git-Python/Monday/qa-metrics-calculator/hello_qa.py
@@ -0,0 +1,15 @@
+import sys
+
+# TODO 1: Ask the user for their name using input()
+name = input("Enter your name: ")
+
+# TODO 2: Ask the user for their role
+role = input("Enter your role: ")
+
+# TODO 3: Print a greeting using an f-string
+# Expected: "Welcome, {name}! Your role is {role}."
+print(f"Welcome, {name}! Your role is {role}.")
+
+# TODO 4: Print the Python version
+# Hint: sys.version_info gives you (major, minor, micro)
+print(f"Python version: {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
