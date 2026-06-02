029e90341219f84c00bbc1b0a6fb941f5f8856b8
Jackson Champion <jackson.champion136@gmail.com>
2026-06-02 12:18:11 -0400

    Minor code changes

--- Week1-Git-Python/Monday/exercise_python_basics/hello_qa.py
+++ Week1-Git-Python/Monday/exercise_python_basics/hello_qa.py
@@ -1,15 +1,14 @@
 import sys
 
-name = None
+# TODO 1: Ask the user for their name
 name = input("Enter your name: ")
 
 # TODO 2: Ask the user for their role
-role = None
 role = input("Enter your role: ")
 
 # TODO 3: Print a welcome message using an f-string
 # Expected: "Welcome, {name}! Your role is {role}."
-print(f"Welcome, {name}. Your role is {role}.")
+print(f"Welcome, {name}! Your role is {role}.")
 
 # TODO 4: Print the Python version
 # Hint: sys.version_info gives you (major, minor, micro)
