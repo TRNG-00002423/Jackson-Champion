f5dd9f906996d7c14f10dc032b0fbb7b8cff5e37
Jackson Champion <jackson.champion136@gmail.com>
2026-06-08 10:13:41 -0400

    Catch up any commits

--- Week1-Git-Python/Tuesday/practice Code/vector.py
+++ Week1-Git-Python/Tuesday/practice Code/vector.py
@@ -5,3 +5,5 @@ scores = np.array([85, 92, 78, 95, 88])
 print(scores + 5)      #90 97 83 100 93]
 print(scores / 2)      # [42.5 46.  39.  47.5 44. ]
 print(scores ** 2)     # [7225 8464 6084 9025 7744]
+
+
